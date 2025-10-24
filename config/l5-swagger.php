aacbe363563c06ca9a0dfbc3ebe9ebbf99b7d986
YaTedene01 <yate0798@gmail.com>
2025-10-24 09:09:33 +0000

    commit6

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -12,7 +12,7 @@ return [
                 /*
                  * Route for accessing api documentation interface
                  */
-                'api' => 'api/documentation',
+                'api' => 'api/v1/faye-yatedene/documentation',
             ],
             'paths' => [
                 /*
@@ -91,7 +91,7 @@ return [
             /*
              * Edit to set the api's base path
              */
-            'base' => env('L5_SWAGGER_BASE_PATH', '/api/v1'),
+            'base' => env('L5_SWAGGER_BASE_PATH', '/api/v1/faye-yatedene'),
 
             /*
              * Absolute path to directories that should be excluded from scanning
